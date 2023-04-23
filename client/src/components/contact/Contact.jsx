0d18d63ed948a35c9b94693b78ee7d89d696dbc3
Not Sid <kumarsid6500@gmail.com>
2023-04-23 23:38:03 +0530

    added scss to contact and made responsive

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -1,18 +1,69 @@
-import React from 'react'
-import {motion} from 'framer-motion'
+import React from "react";
+import { motion } from "framer-motion";
+import burger from "../../assets/burger2.png";
 
 const Contact = () => {
-  return (
-    <section className='contact'>
-      <motion.form >
-        <h2>Contact Us</h2>
-        <input type="text" placeholder='Name'/>
-        <input type="email" placeholder='Email'/>
-        <textarea name="" placeholder='Message...' id="" cols="30" rows="10"></textarea>
-        <button type='submit'>Send</button>
-      </motion.form>
-    </section>
-  )
-}
+	return (
+		<section className="contact">
+			<motion.form
+				initial={{
+					x: "-100vw",
+					opacity: 0,
+				}}
+				animate={{
+					x: 0,
+					opacity: 1,
+				}}
+				transition={{
+					delay: 0.2,
+				}}
+			>
+				<h2>Contact Us</h2>
+				<input type="text" placeholder="Name" />
+				<input type="email" placeholder="Email" />
+				<textarea
+					name=""
+					placeholder="Message..."
+					id=""
+					cols="30"
+					rows="10"
+				></textarea>
+				<button type="submit">Send</button>
+			</motion.form>
 
-export default Contact
+			<motion.div className="formBorder"
+        initial={{
+					x: "100vw",
+					opacity: 0,
+				}}
+				animate={{
+					x: 0,
+					opacity: 1,
+				}}
+				transition={{
+					delay: 0.2,
+				}}
+      >
+				<motion.div
+          initial={{
+            y: "-100vh",
+            x: "50%",
+            opacity: 0,
+          }}
+          animate={{
+            x:"50%",
+            y: "-50%",
+            opacity: 1,
+          }}
+          transition={{
+            delay: 0.6,
+          }}
+        >
+					<img src={burger} alt="burger" />
+				</motion.div>
+			</motion.div>
+		</section>
+	);
+};
+
+export default Contact;
