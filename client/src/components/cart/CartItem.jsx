b66606e427aa803faa89c289deaf3eabaa718702
Not Sid <kumarsid6500@gmail.com>
2023-04-24 23:55:15 +0530

    added cart css

--- client/src/components/cart/CartItem.jsx
+++ client/src/components/cart/CartItem.jsx
@@ -4,13 +4,15 @@ const CartItem = ({ value, title, img, increment, decrement }) => {
 	return (
 		<div>
 			<div className="cartItem">
-				<h4>{title}</h4>
-				<img src={img} alt="burgers" />
-			</div>
-			<div>
-				<button onClick={decrement}> - </button>
-				<input type="number" readOnly value={value} />
-				<button onClick={increment}> + </button>
+				<div>
+					<h4>{title}</h4>
+					<img src={img} alt="burgers" />
+				</div>
+				<div>
+					<button onClick={decrement}> - </button>
+					<input type="number" readOnly value={value} />
+					<button onClick={increment}> + </button>
+				</div>
 			</div>
 		</div>
 	);
